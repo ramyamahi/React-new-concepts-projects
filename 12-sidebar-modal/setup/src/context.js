235241274f5f3ff1ee47e5a2781a8205079d36e9
ramyamahi <ramyamahendran96@gmail.com>
2021-05-14 13:00:42 +0530

    Sidebar modal project - useContext

---
+++ 12-sidebar-modal/setup/src/context.js
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false)
+    const [ isModalOpen, setIsModalOpen ] = useState(false)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = () => {
+        console.log("hgj")
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    return <AppContext.Provider value={{isSidebarOpen, isModalOpen, openSidebar, closeSidebar, openModal, closeModal}}>{children}</AppContext.Provider>
+}
+
+// Custom hook
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
